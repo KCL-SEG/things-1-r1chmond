83e05b2ef7b6eb414c5615083f02fbf55902da21
richmond <richmond.bobie@outlook.com>
2022-10-10 19:34:34 +0100

    Refine the thing model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,21 @@
+
 from django.db import models
+from enum import unique
+from things.views import validate_integer
 
 class Thing(models.Model):
-    name = models.CharField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(
+        max_length = 30, 
+        blank = False,
+        unique = True,
+        )
+    description = models.TextField(
+        max_length = 120, 
+        blank = True, 
+        unique = False,
+    )
+    quantity = models.IntegerField(
+        unique = False,
+        validators = [validate_integer]
+    )
 
